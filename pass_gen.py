3e40d8e40e5dc88aa6cb445364e50592a08eab10
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2023-12-05 22:55:01 +0700

    Add files via upload

---
+++ pass_gen.py
@@ -0,0 +1,7 @@
+Generated Password Parts: ['Lb#\\)', 'Pnsk%2', '%pr^q0tEF[']
+Shuffled Password: Fn0\[%b2qr)tkP#^L%sEp
+Password Length: 21
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
