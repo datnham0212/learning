ed336710180b4e8f01ec0da4af0e26d49f8ac337
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-08-09 22:55:09 +0700

    Create SimpleQuizzes.cs

---
+++ C#/SimpleQuizzes.cs
@@ -0,0 +1,9 @@
+using System;
+class SimpleQuizzes {
+  static void Main() {
+      Console.WriteLine("What's your name? ");
+      string name = Console.ReadLine();
+      
+      Console.WriteLine("Hi there " + name);
+  }
+}
