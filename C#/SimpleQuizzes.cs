fdc5418e0c1ded0d077c3e1541928563d5b9eb94
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-08-09 23:00:31 +0700

    Update SimpleQuizzes.cs

--- C#/SimpleQuizzes.cs
+++ C#/SimpleQuizzes.cs
@@ -1,9 +1,19 @@
 using System;
+
 class SimpleQuizzes {
   static void Main() {
       Console.WriteLine("What's your name? ");
       string name = Console.ReadLine();
       
-      Console.WriteLine("Hi there " + name);
+      Console.WriteLine("Hi there " + name + ", can you do mental math? ");
+      string answer = Console.ReadLine();
+      
+      if(answer=="Yes" || answer=="y" || answer=="yes" || answer == "Y" || answer == "sure" || answer == "Sure"){
+          Console.WriteLine("Ok then, let's play");
+      }
+      
+      else{
+          Console.WriteLine("Maybe next time then");
+      }
   }
 }
