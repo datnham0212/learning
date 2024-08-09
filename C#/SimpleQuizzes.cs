7d539e57e86087400e8f94267df189fba46fb642
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-08-09 23:11:02 +0700

    Update SimpleQuizzes.cs

--- C#/SimpleQuizzes.cs
+++ C#/SimpleQuizzes.cs
@@ -1,5 +1,22 @@
 using System;
 
+
+class mathQuestion {
+    int a = 1;
+    int b = 2;
+    
+    public void question(){
+        Console.WriteLine("What's 1 + 2? ");
+        int userAnswer = Convert.ToInt32(Console.ReadLine());
+        int actualAnswer = a + b;
+        
+        string result = (userAnswer == actualAnswer) ? "Correct" : "Wrong";
+        
+        Console.WriteLine(result);
+    }
+}
+
+
 class SimpleQuizzes {
   static void Main() {
       Console.WriteLine("What's your name? ");
@@ -10,6 +27,9 @@ class SimpleQuizzes {
       
       if(answer=="Yes" || answer=="y" || answer=="yes" || answer == "Y" || answer == "sure" || answer == "Sure"){
           Console.WriteLine("Ok then, let's play");
+          
+          mathQuestion mq = new mathQuestion();
+          mq.question();
       }
       
       else{
